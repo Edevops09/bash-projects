b024adea2ef66b33e5c2385457dd5afbd0fdf2f3
Elyas Syed <isyedwork@gmail.com>
2022-06-04 20:39:38 +0000

    added variable example

--- scripts/at_script.sh
+++ scripts/at_script.sh
@@ -11,3 +11,15 @@ touch at_dir/file{1..5}.txt
 # at 9am 12/20/2021 -f at_script.sh
 # at now + 5 minutes -f at_script.sh
 # at now + 2 days -f at_script.sh
+
+Declaring a Variable
+
+#!/bin/bash
+
+name="Mike"
+age=35
+zip_code=55465
+
+echo "Name is $name"
+echo "Age is $age"
+echo "zip code is $zip_code"
