7682a6972a820af0aa3f0ceb3f99271a58b89552
Elyas09 <isyedwork@gmail.com>
2022-06-24 03:49:17 +0000

    modified get-pod.sh

--- scripts/get-pod.sh
+++ scripts/get-pod.sh
@@ -2,9 +2,9 @@
 # Script looks and searches for Kubernetes pods
 
 echo "Hello, which pod or tool do you want to find?"
-read -r POD_NAME
+read -r USER_INPUT
 
 # Filters or greps for the pod name from all the pods
-ALL_PODS="$(kubectl get pods --all-namespaces | awk '{print $1}' | grep -v name | grep "$POD_NAME")"
+ALL_PODS="$(kubectl get pods --all-namespaces | awk '{print $1}' | grep -v name | grep "$USER_INPUT")"
 
 echo "$ALL_PODS"
